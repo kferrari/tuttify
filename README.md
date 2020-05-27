bc2ffc7296c84a1e11f0167ace699ebc78272cbd
Kim Ferrari <k.ferrari@me.com>
2020-05-27 09:37:30 +0200

    Extended functionality
    + allow multi-word queries
    + --silent flag for run without notification
    + --canton flag for specific search
    + --maxprice flag to set highest price to notify

--- README.md
+++ README.md
@@ -4,15 +4,41 @@ Tired of being notified too late about new listings of your favourite things?
 
 Use tuttify.py to scrape tutti.ch for new listings and alert you via Telegram.
 
-### Prerequisites
+### Install
 
-* clone this repo or download tuttify.py
-* edit lines 18 and 19 in tuttify.py to include your name and email address, if you want
-* install [telegram-send](https://pypi.org/project/telegram-send/) and set up your personal bot for notification
-* install [beautifulsoup](https://pypi.org/project/beautifulsoup4/)
+* Clone this repo or download tuttify.py.
+* Edit lines 18 and 19 in tuttify.py to include your name and email address, if you want. This is good practice in case someone over at tutti.ch wants to contact you.
+* Install [telegram-send](https://pypi.org/project/telegram-send/) and run `telegram-send --configure` to set up your personal Bot.
+* Install [beautifulsoup](https://pypi.org/project/beautifulsoup4/).
 
 ### Usage
 
+Simple run:
+
+```bash
+python3 tuttify.py -q sofa
 ```
-python3 tuttify.py sofa
+
+To run in background:
+
+```bash
+nohup python3 -u tuttify.py -q sofa &
+```
+
+The first run will build the dictionary of existing listings and save it to a JSON file. It also sends a message for each listing. To only build the dictionaryof existing listing, use the `--silent` flag.
+
+```bash
+python3 tuttify.py -q sofa --silent
+```
+
+To only search a specific canton, use the `--canton` parameter
+
+```bash
+python3 tuttify.py -q sofa --canton graubuenden
+```
+
+To define a maximum price, use `--maxprice`
+
+```bash
+python3 tuttify.py -q sofa --canton graubuenden --maxprice 50
 ```
--- tuttify.py
+++ tuttify.py
@@ -10,20 +10,25 @@ from bs4 import BeautifulSoup
 parser = argparse.ArgumentParser(
 description="This script will watch tutti.ch for new ads and notify you via telegram. Also, it keeps a record of listings with prices in a JSON file."
 )
-parser.add_argument('query', metavar="Query", type=str, help='tutti.ch search string')
+parser.add_argument('-c', '--canton', metavar="Canton", type=str, default="ganze-schweiz",
+    help="In which canton to look for.")
+parser.add_argument('-q', '--query', metavar="Query", type=str, help='tutti.ch search string')
+parser.add_argument('-s', '--silent', action='store_true', default=False,
+    help="Don't send notifications.")
+parser.add_argument('-m', '--maxprice', metavar="Price", type=int, default=0,
+    help="Highest price to still look for.")
 args = parser.parse_args()
 
 # Header for requests
 headers = {
     'User-Agent': 'Anon',
-    'From': 'youre.mail@here.com'
+    'From': 'your.email@here.com'
 }
 
 while True:
     # Download page
     try:
-        search_url = 'https://www.tutti.ch/de/li/ganze-schweiz?q=' + args.query.lower()
-        print(search_url)
+        search_url = 'https://www.tutti.ch/de/li/' + args.canton.lower() + '?q=' + args.query.lower().replace(" ", "%20")
         html = urlopen(search_url)
         soup = BeautifulSoup(html, features="lxml")
 
@@ -37,11 +42,18 @@ while True:
             title = item.find_all('h4', attrs={"class":"_2SE_L"})[0].get_text()
 
             price = item.find_all('div', attrs={"class":"_6HJe5"})[0].get_text()
+            price_num = price[:-2].replace("'", "")
+
+            try:
+                price_num = int(price_num)
+                isTooExp = (price_num > args.maxprice) and (args.maxprice > 0)
+            except:
+                print("Invalid price. Probably empty.")
+                isTooExp = False
 
             location = item.find_all('span', attrs={"class":"_3f6Er"})[0].get_text()
 
             # Create dict for first advertisement
-            #new_dict = {"name" : title, "url" : url, "price" : price, "date" : date_pub}
             new_dict = {"name" : title, "url" : url, "price": price, "location": location}
 
             # Check if file exists already and create if not
@@ -54,6 +66,12 @@ while True:
 
                 with open(fname, 'w') as f:
                     json.dump(mydict, f)
+
+                # notify
+                print("New listing...")
+                if (not args.silent) and (not isTooExp):
+                    message = 'Neues Inserat: {} ({}) in {}\n {}'.format(title, price, location, url)
+                    telegram_send.send(messages=[message])
 
             else:
                 with open(fname,'r+') as f:
@@ -72,8 +90,9 @@ while True:
 
                         # notify
                         print("New listing...")
-                        message = 'Neues Inserat: {} ({}) in {}\n {}'.format(title, price, location, url)
-                        telegram_send.send(messages=[message])
+                        if (not args.silent) and (not isTooExp):
+                            message = 'Neues Inserat: {} ({}) in {}\n {}'.format(title, price, location, url)
+                            telegram_send.send(messages=[message])
 
             # Close file
             f.close()
